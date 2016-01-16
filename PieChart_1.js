056a9179da175e868d0e7192dd189286716aa4e3
Lassi Pispa <lassi.pispa@live.com>
2016-01-16 16:49:03 +0200

    Second commit
    Edited styling and comments

--- PieChart_1.js
+++ PieChart_1.js
@@ -1,27 +1,27 @@
-//. A pie chart .//
-// Pie Chart data set:
+// Chart.js pie chart
 var pieData = [
-{
-	value: 300,
-	color:"#F7464A",
-	highlight: "#FF5A5E",
-	label: "Sosiaali- ja terveysmenot"
-},
-{
-	value: 50,
-	color: "#46BFBD",
-	highlight: "#5AD3D1",
-	label: "Puolustusvoimat"
-},
-{
-	value: 100,
-	color: "#FDB45C",
-	highlight: "#FFC870",
-	label: "Yellow"
-}
-];
-var pieChart_1 = document.getElementById("Chart_1").getContext("2d");
-new Chart(pieChart_1).Pie(pieData,{
-	responsive: false,
-	maintainAspectRatio: true
+	// Pie Chart data set:
+	{
+		value: 300,
+		color:"#F7464A",
+		highlight: "#FF5A5E",
+		label: "Sosiaali- ja terveysmenot"
+	},
+	{
+		value: 50,
+		color: "#46BFBD",
+		highlight: "#5AD3D1",
+		label: "Puolustusvoimat"
+	},
+	{
+		value: 100,
+		color: "#FDB45C",
+		highlight: "#FFC870",
+		label: "Yellow"
+	}
+	];
+	var pieChart_1 = document.getElementById("Chart_1").getContext("2d");
+	new Chart(pieChart_1).Pie(pieData,{
+		responsive: false,
+		maintainAspectRatio: true
 });

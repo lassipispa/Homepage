57ad1d2d527d05e95311fb4329e71745a62b36d5
lassipispa <lassi.pispa@live.com>
2016-01-16 14:58:50 +0200

    Firts commit

---
+++ PieChart_1.js
@@ -0,0 +1,27 @@
+//. A pie chart .//
+// Pie Chart data set:
+var pieData = [
+{
+	value: 300,
+	color:"#F7464A",
+	highlight: "#FF5A5E",
+	label: "Sosiaali- ja terveysmenot"
+},
+{
+	value: 50,
+	color: "#46BFBD",
+	highlight: "#5AD3D1",
+	label: "Puolustusvoimat"
+},
+{
+	value: 100,
+	color: "#FDB45C",
+	highlight: "#FFC870",
+	label: "Yellow"
+}
+];
+var pieChart_1 = document.getElementById("Chart_1").getContext("2d");
+new Chart(pieChart_1).Pie(pieData,{
+	responsive: false,
+	maintainAspectRatio: true
+});
